bbff97114c3e2d96cedd7dbaedf5e84a4701c6ec
Jozef Podlecki <jozef.witold.podlecki@gmail.com>
2020-11-27 23:53:00 +0000

    add search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React, { FunctionComponent } from "react";
+import React, { ChangeEvent, FunctionComponent, useState } from "react";
 import { Route, Switch } from "react-router";
 
-const App: FunctionComponent = () => {
+type Props = {
+    value: string;
+    onSearch(value: string): void;
+}
+
+const SearchBox: FunctionComponent<Props> = ({
+    value,
+    onSearch
+}) => {
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.currentTarget.value;
+        onSearch(value);
+    }
+
     return <div>
-        
+        <input type="text" value={value} onChange={onChange}/>
+    </div>
+}
+
+const App: FunctionComponent = () => {
+    const [value, setValue] = useState("");
+
+    const onSearch = (value: string) => {
+        setValue(value)
+    }
+
+    return <div>
+        <SearchBox value={value} onSearch={onSearch}/>
+        <div>
+        </div>
     </div>
 };
 
