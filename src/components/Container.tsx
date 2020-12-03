ffcc541b9df03b623ef375e437dacd37867d1b6d
Jozef Podlecki <jozef.witold.podlecki@gmail.com>
2020-12-03 23:13:18 +0000

    add loader and data

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,21 +6,66 @@ import { ImageItem } from "models/ImageItem";
 
 
 import styles from "./container.scss";
+import Loader from "react-loader-spinner";
 
 type Props = {
 }
 
 const Container: FunctionComponent<Props> = ({
 }) => {
-    const [items, setItems] = useState<ImageItem[]>([]);
+    const [{
+        items,
+        page,
+        prevY,
+        isLoading
+    }, setState] = useState<{
+        items: ImageItem[];
+        page: string;
+        prevY: number;
+        isLoading: boolean;
+    }>({
+        items: [],
+        page: "",
+        prevY: 0,
+        isLoading: true,
+    });
     
     useEffect(() => {
-        getFlags()
+        getFlags({page})
             .then(result => {
-                setItems(result.slice(0, 5));
+                setState(state => ({
+                    ...state,
+                    items: result.slice(0, 5),
+                    isLoading: false,
+                }));
             })
     }, []);
-    console.log(items);
+    
+    const options = {
+        root: null as any,
+        rootMargin: "0px",
+        threshold: 1.0
+      };
+
+
+    const handleObserver = (entities, observer) => {
+        const y = entities[0].boundingClientRect.y;
+
+        if (prevY > y) {
+            const lastItem = items[items.length - 1];
+            const curPage = lastItem.id;
+            getFlags({page})
+            setState({ page: curPage });
+        }
+
+        setState({ prevY: y });
+    }
+
+    const observer = new IntersectionObserver(
+        handleObserver,
+        options
+    );
+
     return <div className={styles.container}>
         {items.map(pr => <div key={pr.id} className={styles.item}>
             <div
@@ -31,6 +76,7 @@ const Container: FunctionComponent<Props> = ({
                     <span>Test</span>
                 </div>
         </div>)}
+        {isLoading ? <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} /> : null}
     </div>
 }
 
